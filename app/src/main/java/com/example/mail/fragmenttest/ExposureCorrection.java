8f2cf7f5560010fe1c17b3ebecd09fe639ce5e30
Buntepixel <christian@buntepixel.eu>
2017-05-12 00:14:07 +0200

    changed exposure bar script

--- app/src/main/java/com/example/mail/fragmenttest/ExposureCorrection.java
+++ app/src/main/java/com/example/mail/fragmenttest/ExposureCorrection.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -13,8 +14,8 @@ import android.view.View;
 public class ExposureCorrection extends View {
     private int mNbStrokes = 19;
     private int mStrokeGap = 15;
-    private int mWidth;
 
+    private static final String TAG = ExposureCorrection.class.getSimpleName();
 
     public ExposureCorrection(Context context) {
         super(context);
@@ -36,40 +37,62 @@ public class ExposureCorrection extends View {
     }
 
 
-
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int height = 16;
+        int height = 20;
         //calculate the view width
         int calculatedWidth = (mNbStrokes * mStrokeGap);
+
         int width = calculatedWidth;
 
         setMeasuredDimension(width, height);
     }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Paint paint = new Paint();
         //set the color for the dot that you want to draw
+        paint.setAntiAlias(true);
         paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(6);
+        paint.setStrokeWidth(mStrokeGap / 4);
         paint.setStrokeCap(Paint.Cap.SQUARE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+
 
         //function to create dot
         createLine(canvas, paint);
         //createExposureContrVis(canvas, paint, 8);
     }
+
     private void createLine(Canvas canvas, Paint paint) {
         int width = getWidth();
         int height = getHeight();
+        int  startLine, endLine;
         //strokes next to each other
         for (int i = 0; i < mNbStrokes; i++) {
-            paint.setARGB(255, 70, 70, 70);
-            if (i % 3 == 0) {
+            startLine = height / 3;
+            endLine = startLine*2;
+            paint.setARGB(255, 90, 90, 90);
+            if (i == mNbStrokes / 2) {
+                paint.setARGB(255, 70, 255, 50);
+                startLine = height ;
+                endLine = 0;
+                Log.d(TAG, String.format("i: %d myNbstrokes: %d", i, mNbStrokes));
+            } else if (i % 3 == 0) {
                 paint.setARGB(255, 200, 200, 200);
+                startLine = height /4;
+                endLine = startLine*3;
+
             }
-            canvas.drawLine(0 + (i * (width / mNbStrokes)), 0, 0 + (i * (width / mNbStrokes)), height / 2, paint);
+
+
+            //Log.d(TAG, String.format("width: %d height: %d i: %d ", width, height, i));
+            //canvas.drawLine((i * (width / mNbStrokes)), 0, (i * (width / mNbStrokes)), height , paint);
+            canvas.drawLine((i * (mStrokeGap)), startLine, (i * (mStrokeGap)), endLine , paint);
+
+
         }
     }
 
