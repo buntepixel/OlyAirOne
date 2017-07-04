c5e8b7a660af7829c1986b711a0a7c4b59500fdb
Buntepixel <christian@buntepixel.eu>
2017-07-04 22:28:58 +0200

    mode button switches image resource

--- app/src/main/java/com/example/mail/fragmenttest/MainActivity.java
+++ app/src/main/java/com/example/mail/fragmenttest/MainActivity.java
@@ -6,17 +6,19 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity
-        implements TriggerFragment.OnShutterReleasePressed, MainSettingsFragment.OnFragmentInteractionListener{
+        implements TriggerFragment.OnShutterReleasePressed, MainSettingsFragment.OnFragmentInteractionListener {
     private static final String TAG = MainActivity.class.getSimpleName();
     ExposureFragment fExposure;
     ApartureFragment fAparture;
     TriggerFragment fTrigger;
     MainSettingsFragment fMainSettings;
     FragmentManager fm = getSupportFragmentManager();
-
+    int[] modeArr = new int[]{R.drawable.ic_aparturemode, R.drawable.ic_shuttermode, R.drawable.ic_manualmode, R.drawable.ic_videomode};
 
 
     @Override
@@ -24,30 +26,35 @@ public class MainActivity extends FragmentActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        final ImageButton ib_RecordMode = (ImageButton) findViewById(R.id.ib_RecordMode);
+        ib_RecordMode.setOnClickListener(new View.OnClickListener() {
+            int modeCounter = 0;
 
+            @Override
+            public void onClick(View v) {
+
+                modeCounter++;
+                Toast.makeText(MainActivity.this, "Hello" + modeCounter, Toast.LENGTH_SHORT).show();
+                ib_RecordMode.setImageResource(modeArr[modeCounter % (modeArr.length - 1)]);
+
+            }
+        });
+
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
         Log.d(TAG, "start");
         //check for Trigger container
-    if (findViewById(R.id.fl_FragCont_Trigger) != null){
-        Log.d(TAG, "if");
-    }
+
         fTrigger = new TriggerFragment();
         fExposure = new ExposureFragment();
         fAparture = new ApartureFragment();
         fMainSettings = new MainSettingsFragment();
-
-
-        //fTrigger.setArguments(getIntent().getExtras());
-        // fAparture.setArguments(getIntent().getExtras());
-        //fExposure.setArguments(getIntent().getExtras());
-        fragmentTransaction.add(R.id.fl_FragCont_ExpApart1, fExposure,"Expo");
+        fragmentTransaction.add(R.id.fl_FragCont_ExpApart1, fExposure, "Expo");
         //fragmentTransaction.add(R.id.fl_FragCont_ExpApart2, fAparture, "Apart");
         fragmentTransaction.add(R.id.fl_FragCont_Trigger, fTrigger);
-        fragmentTransaction.add(R.id.fl_FragCont_MainSettings,fMainSettings);
+        fragmentTransaction.add(R.id.fl_FragCont_MainSettings, fMainSettings);
 
         fragmentTransaction.commit();
     }
-
 
 
     @Override
@@ -68,7 +75,7 @@ public class MainActivity extends FragmentActivity
 
         FragmentTransaction ft = fm.beginTransaction();
         ft.setCustomAnimations(R.anim.slidedown, R.anim.slideup);
-        ft.replace(R.id.fl_FragCont_ExpApart1, fAparture,"Apart");
+        ft.replace(R.id.fl_FragCont_ExpApart1, fAparture, "Apart");
         ft.addToBackStack(fAparture.toString());
         ft.commit();
     }
