8a1ed74e7d8bd61f1342ce72c3e8920ed135277f
Buntepixel <christian@buntepixel.eu>
2017-10-07 11:33:51 +0200

    live view fragment gets called after cam connection established.

--- app/src/main/java/com/example/mail/fragmenttest/CameraActivity.java
+++ app/src/main/java/com/example/mail/fragmenttest/CameraActivity.java
@@ -1,7 +1,12 @@
 package com.example.mail.fragmenttest;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -11,6 +16,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 import jp.co.olympus.camerakit.OLYCamera;
 import jp.co.olympus.camerakit.OLYCameraConnectionListener;
 import jp.co.olympus.camerakit.OLYCameraKitException;
@@ -18,13 +29,15 @@ import jp.co.olympus.camerakit.OLYCameraKitException;
 
 public class CameraActivity extends FragmentActivity
         implements TriggerFragment.OnTriggerFragmInteractionListener, LiveViewFragment.OnLiveViewInteractionListener,
-        OLYCameraConnectionListener{
+        OLYCameraConnectionListener {
     private static final String TAG = CameraActivity.class.getSimpleName();
 
-    Boolean isActive = true;
+    //Boolean isActive = false;
+    private Executor connectionExecutor = Executors.newFixedThreadPool(1);
     int currDriveMode = 0;
     String currExpApart1;
     String currExpApart2;
+    FragmentManager fm;
     TriggerFragment fTrigger;
     LiveViewFragment fLiveView;
     public static OLYCamera camera = null;
@@ -44,7 +57,7 @@ public class CameraActivity extends FragmentActivity
 
     @Override
     public void onDisconnectedByError(OLYCamera olyCamera, OLYCameraKitException e) {
-        Toast.makeText(this,"Connection to Camera Lost, please Reconnect", Toast.LENGTH_SHORT ).show();
+        Toast.makeText(this, "Connection to Camera Lost, please Reconnect", Toast.LENGTH_SHORT).show();
         finish();
     }
 
@@ -75,14 +88,14 @@ public class CameraActivity extends FragmentActivity
         if (savedInstanceState != null) {
             return;
         }
+        camera = new OLYCamera();
+        camera.setContext(getApplicationContext());
+        camera.setConnectionListener(this);
         //add Trigger,LiveView Fragment
         fTrigger = new TriggerFragment();
         fLiveView = new LiveViewFragment();
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-        fragmentTransaction.add(R.id.fl_FragCont_Trigger, fTrigger, "Trigger");
-        fragmentTransaction.add(R.id.fl_FragCont_cameraLiveImageView, fLiveView, "LiveView");
-        fragmentTransaction.commit();
+        fm = getSupportFragmentManager();
+
 
 
     }
@@ -95,12 +108,171 @@ public class CameraActivity extends FragmentActivity
 
     @Override
     protected void onResume() {
-            super.onResume();
+        super.onResume();
+        startConnectingCamera();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.add(R.id.fl_FragCont_Trigger, fTrigger, "Trigger");
+
+        fragmentTransaction.commit();
     }
 
     @Override
     protected void onPause() {
-            super.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    @Override
+    public void onTriggerFragmInteraction(int settingsType) {
+        // Toast.makeText(getParent(), settingsType, Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "settingsType: " + settingsType);
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        //Log.d(TAG,"visFrag"+ fm.getFragments().toString());
+        int fragLayout;
+        Log.d(TAG, "currdriveMode: " + currDriveMode);
+        if (currDriveMode == 4 && settingsType <= 1) {
+            ExposurePressed(ft, R.id.fl_FragCont_ExpApart2, 2);
+            AparturePressed(ft);
+            //Log.d(TAG, "A_nr1: " + currExpApart1 + " nr2: " + currExpApart2);
+            ft.commit();
+            //Log.d(TAG, "B_nr1: " + currExpApart1 + " nr2: " + currExpApart2);
+        } else {
+            switch (settingsType) {
+                case 0:
+                    currExpApart1 = ExposurePressed(ft, R.id.fl_FragCont_ExpApart1, 1);
+                    break;
+                case 1:
+                    AparturePressed(ft);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    IsoPressed(ft);
+                    break;
+                case 4:
+                    WbPressed(ft);
+                    break;
+            }
+            //ft.addToBackStack("back");
+            ft.commit();
+            //Log.d(TAG, "C_nr1: " + currExpApart1 + " nr2: " + currExpApart2);
+        }
+    }
+
+    private void startConnectingCamera() {
+        connectionExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(CameraActivity.this);
+
+                try {
+                    camera.connect(OLYCamera.ConnectionType.WiFi);
+                } catch (OLYCameraKitException e) {
+                    alertConnectingFailed(e);
+                    return;
+                }
+              /*  try {
+                    camera.changeLiveViewSize(toLiveViewSize(preferences.getString("live_view_quality", "QVGA")));
+                } catch (OLYCameraKitException e) {
+                    Log.w(TAG, "You had better uninstall this application and install it again.");
+                    alertConnectingFailed(e);
+                    return;
+                }*/
+                try {
+                    camera.changeRunMode(OLYCamera.RunMode.Recording);
+                } catch (OLYCameraKitException e) {
+                    alertConnectingFailed(e);
+                    return;
+                }
+
+                // Restores my settings.
+                if (camera.isConnected()) {
+                    Map<String, String> values = new HashMap<String, String>();
+                    for (String name : Arrays.asList(
+                            "TAKEMODE",
+                            "TAKE_DRIVE",
+                            "APERTURE",
+                            "SHUTTER",
+                            "EXPREV",
+                            "WB",
+                            "ISO",
+                            "RECVIEW"
+                    )) {
+                        String value = preferences.getString(name, null);
+                        if (value != null) {
+                            values.put(name, value);
+                        }
+                    }
+                    if (values.size() > 0) {
+                        try {
+                            camera.setCameraPropertyValues(values);
+                        } catch (OLYCameraKitException e) {
+                            Log.w(TAG, "To change the camera properties is failed: " + e.getMessage());
+                        }
+                    }
+                }
+
+                if (!camera.isAutoStartLiveView()) { // Please refer a document about OLYCamera.autoStartLiveView.
+                    // Start the live-view.
+                    // If you forget calling this method, live view will not be displayed on the screen.
+                    try {
+                        camera.startLiveView();
+                    } catch (OLYCameraKitException e) {
+                        Log.w(TAG, "To start the live-view is failed: " + e.getMessage());
+                        return;
+                    }
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        onConnectedToCamera();
+                    }
+                });
+            }
+        });
+    }
+
+
+    private void alertConnectingFailed(Exception e) {
+        final Intent myIntent = new Intent(this, ConnectToCamActivity.class);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle("Connect failed")
+                .setMessage(e.getMessage() != null ? e.getMessage() : "Unknown error")
+                .setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        //startScanningCamera();
+                        startActivity(myIntent);
+                    }
+                });
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                builder.show();
+            }
+        });
+    }
+
+    private void onConnectedToCamera() {
+        Log.d(TAG, "Connected to Cam");
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.add(R.id.fl_FragCont_cameraLiveImageView, fLiveView, "LiveView");
+        fragmentTransaction.commit();
+           /* LiveViewFragment fragment = new LiveViewFragment();
+            fragment.setCamera(camera);
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment1, fragment);
+            transaction.commitAllowingStateLoss();*/
+
     }
 
     void SetMainSettingsButtons(int mode) {
@@ -169,45 +341,6 @@ public class CameraActivity extends FragmentActivity
             currExpApart2 = "";
         }
         ft.commit();
-    }
-
-    @Override
-    public void onTriggerFragmInteraction(int settingsType) {
-        // Toast.makeText(getParent(), settingsType, Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "settingsType: " + settingsType);
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-
-        //Log.d(TAG,"visFrag"+ fm.getFragments().toString());
-        int fragLayout;
-        Log.d(TAG, "currdriveMode: " + currDriveMode);
-        if (currDriveMode == 4 && settingsType <= 1) {
-            ExposurePressed(ft, R.id.fl_FragCont_ExpApart2, 2);
-            AparturePressed(ft);
-            //Log.d(TAG, "A_nr1: " + currExpApart1 + " nr2: " + currExpApart2);
-            ft.commit();
-            //Log.d(TAG, "B_nr1: " + currExpApart1 + " nr2: " + currExpApart2);
-        } else {
-            switch (settingsType) {
-                case 0:
-                    currExpApart1 = ExposurePressed(ft, R.id.fl_FragCont_ExpApart1, 1);
-                    break;
-                case 1:
-                    AparturePressed(ft);
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    IsoPressed(ft);
-                    break;
-                case 4:
-                    WbPressed(ft);
-                    break;
-            }
-            //ft.addToBackStack("back");
-            ft.commit();
-            //Log.d(TAG, "C_nr1: " + currExpApart1 + " nr2: " + currExpApart2);
-        }
     }
 
     private String ExposurePressed(FragmentTransaction ft, int FrameLayout, int FrameLayoutId) {
