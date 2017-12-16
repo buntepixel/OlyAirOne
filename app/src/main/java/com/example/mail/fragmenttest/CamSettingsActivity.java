0094ab55b11c2d6c08209633aabd2fd6a7da4220
Buntepixel <christian@buntepixel.eu>
2017-12-16 21:18:52 +0100

    working on CamSettings

--- app/src/main/java/com/example/mail/fragmenttest/CamSettingsActivity.java
+++ app/src/main/java/com/example/mail/fragmenttest/CamSettingsActivity.java
@@ -3,19 +3,24 @@ package com.example.mail.fragmenttest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
 import android.widget.ExpandableListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class CamSettingsActivity extends AppCompatActivity {
+    private static final String TAG = CamSettingsActivity.class.getSimpleName();
+
     List<String> groupList;
     List<String> childList;
-    Map<String, List<String>> laptopCollection;
+    Map<String, List<String>> categoryColl;
     ExpandableListView expListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,76 +29,74 @@ public class CamSettingsActivity extends AppCompatActivity {
         createGroupList();
 
         createCollection();
-        ArrayList<String> CONTINUOUS_SHOOTING_VELOCITY= new ArrayList<String>(Arrays.asList("1fps","2fps","3fps","4fps","5fps","6fps","7fps","8fps","9fps","10fps"));
-        ArrayList<String> IMAGESIZE= new ArrayList<String>(Arrays.asList("4608x3456","3200x2400","2560x1920","1920x1440","1600x1200","1280x960","1024x768","640x480"));
         Boolean rawImageSaving;
-        ArrayList<String> jpecCompressionRatio= new ArrayList<String>(Arrays.asList("Super Fine","Fine","Normal","Basic"));
-        ArrayList<String> movieQuality= new ArrayList<String>(Arrays.asList("Full HD (MOV, 1920x1080, Fine Quality)","Full HD (MOV, 1920x1080, Normal Quality)"
-                ,"HD (MOV, 1280x720, Fine Quality)","HD (MOV, 1280x720, Normal Quality)"," Clip Full HD (1920x1080)"));
-        ArrayList<String> clipRecordTime = new ArrayList<String>(Arrays.asList("1sec","2sec","3sec","4sec","5sec","6sec","7sec","8sec"));
-
-        ArrayList<String> imageSavingDest= new ArrayList<String>(Arrays.asList("DESTINATION_FILE_MEDIA","DESTINATION_FILE_WIFI"));
-
-
-
-
 
         expListView = (ExpandableListView) findViewById(R.id.laptop_list);
         final ExpandableListAdapter expListAdapter = new ExpandableListAdapter(
-                this, groupList, laptopCollection);
+                this, groupList, categoryColl);
         expListView.setAdapter(expListAdapter);
+
+        Log.d(TAG, "What is it: " + expListAdapter.getChild(1, 2));
+
 
         //setGroupIndicatorToRight();
 
-     /*   expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
 
             public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
                 final String selected = (String) expListAdapter.getChild(groupPosition, childPosition);
                 Toast.makeText(getBaseContext(), selected, Toast.LENGTH_LONG).show();
                 return true;
             }
-        });*/
+        });
     }
+
     private void createGroupList() {
         groupList = new ArrayList<String>();
         groupList.add("Auto Exposure Bracketing");
         groupList.add("Image Settings");
+        groupList.add("Movie Settings");
         groupList.add("Focusing");
         groupList.add("Shooting");
 
     }
+
     private void createCollection() {
         // preparing laptops collection(child)
-        String[] AEB = { "setup" };
-        String[] hclModels = { "HCL S2101", "HCL L2102", "HCL V2002" };
-        String[] lenovoModels = { "IdeaPad Z Series", "Essential G Series",
-                "ThinkPad X Series", "Ideapad Z Series" };
-        String[] sonyModels = { "VAIO E Series", "VAIO Z Series",
-                "VAIO S Series", "VAIO YB Series" };
-        String[] dellModels = { "Inspiron", "Vostro", "XPS" };
-        String[] samsungModels = { "NP Series", "Series 5", "SF Series" };
+        String[] AEB = {"setup"};
+        String[] image = {"AspectRatio", "ImageSize", "jpgComression", "ImageDestination", "RawImageSaving"};
+        String[] movie = {"Quality", "ClipRecordTime"};
+        String[] focusing = {"Touch shutter"};
+        String[] shooting = {"contShootinvVel", "Self Timer", ""};
 
-        laptopCollection = new LinkedHashMap<String, List<String>>();
 
-        for (String laptop : groupList) {
-            if (laptop.equals("Auto Exposure Bracketing")) {
+        categoryColl = new LinkedHashMap<String, List<String>>();
+
+        for (String group : groupList) {
+            if (group.equals("Auto Exposure Bracketing"))
                 loadChild(AEB);
-            } else if (laptop.equals("Image Settings"))
-                loadChild(dellModels);
-            else if (laptop.equals("Focusing"))
-                loadChild(sonyModels);
-            else if (laptop.equals("Shooting"))
-                loadChild(hclModels);
-            laptopCollection.put(laptop, childList);
+            else if (group.equals("Image Settings"))
+                loadChild(image);
+            else if (group.equals("Movie Settings"))
+                loadChild(movie);
+            else if (group.equals("Focusing"))
+                loadChild(focusing);
+            else if (group.equals("Shooting"))
+                loadChild(shooting);
+            categoryColl.put(group, childList);
         }
     }
+
     private void loadChild(String[] laptopModels) {
         childList = new ArrayList<String>();
-        for (String model : laptopModels)
+        for (String model : laptopModels) {
+
             childList.add(model);
+        }
     }
+
     private void setGroupIndicatorToRight() {
-		/* Get the screen width */
+        /* Get the screen width */
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
         int width = dm.widthPixels;
@@ -101,6 +104,7 @@ public class CamSettingsActivity extends AppCompatActivity {
         expListView.setIndicatorBounds(width - getDipsFromPixel(35), width
                 - getDipsFromPixel(5));
     }
+
     // Convert pixel to dip
     public int getDipsFromPixel(float pixels) {
         // Get the screen's density scale
--- app/src/main/java/com/example/mail/fragmenttest/ExpandableListAdapter.java
+++ app/src/main/java/com/example/mail/fragmenttest/ExpandableListAdapter.java
@@ -7,11 +7,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -19,8 +23,8 @@ import java.util.Map;
  * Created by mail on 30/11/2017.
  */
 
-public class ExpandableListAdapter extends BaseExpandableListAdapter {
-    private static final String TAG = SettingsFragment.class.getSimpleName();
+public class ExpandableListAdapter extends BaseExpandableListAdapter implements AdapterView.OnItemSelectedListener {
+    private static final String TAG = ExpandableListAdapter.class.getSimpleName();
 
     // 4 Child types
     private static final int CHILD_TYPE_1 = 0;
@@ -32,6 +36,73 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
     private Activity context;
     private Map<String, List<String>> myChilds;
     private List<String> myParents;
+    ArrayList<String> child2ArrList;
+
+    private final Map<String, String> aspectRatio = new HashMap<String, String>() {{
+        put("4:3 ", "04_03");
+        put("3:2", "03_02");
+        put("16:9", "16_09");
+        put("3:4", "03_04");
+        put("1:1", "06_06");
+    }};
+    private final Map<String, String> jpgCompression = new HashMap<String, String>() {
+        {
+            put("Super Fine", "CMP_2_7");
+            put("Fine", "CMP_4");
+            put("Normal", "CMP_8");
+            put("Basic", "CMP_12");
+        }
+    };
+    private final Map<String, String> imageSize = new HashMap<String, String>() {
+        {
+            put("4608x3456", "4608x3456");
+            put("2560x1920", "2560x1920");
+            put("1920x1440", "1920x1440");
+            put("1600x1200", "1600x1200");
+            put("1280x960", "1280x960");
+            put("1024x768", "1024x768");
+            put("640x480", "640x480");
+        }
+    };
+    private final Map<String, String> imageSaveDestination = new HashMap<String, String>() {{
+        put("store on Camera", "DESTINATION_FILE_MEDIA");
+        put("Store on Mobile", "DESTINATION_FILE_WIFI");
+    }};
+
+    private final Map<String, String> movieQuality = new HashMap<String, String>() {{
+        put("Full HD (Fine Quality)", "QUALITY_MOVIE_FULL_HD_FINE");
+        put("Full HD (Normal Quality)", "QUALITY_MOVIE_FULL_HD_NORMAL");
+        put("HD (Fine Quality)", "QUALITY_MOVIE_HD_FINE");
+        put("HD (Normal Quality)", "QUALITY_MOVIE_HD_NORMAL");
+        put("Clip Full HD (1920x1080)", "QUALITY_MOVIE_SHORT_MOVIE");
+    }};
+    private final Map<String, String> clipRecordTime = new HashMap<String, String>() {{
+        put("1 sec", "1");
+        put("2 sec", "2");
+        put("3 sec", "3");
+        put("4 sec", "4");
+        put("5 sec", "5");
+        put("6 sec", "6");
+        put("7 sec", "7");
+        put("8 sec", "8");
+    }};
+
+    private final Map<String, String> continousShootingSpeed = new HashMap<String, String>() {{
+        put("1fps", "1");
+        put("2fps", "2");
+        put("3fps", "3");
+        put("4fps", "4");
+        put("5fps", "5");
+        put("6fps", "6");
+        put("7fps", "7");
+        put("8fps", "8");
+        put("9fps", "9");
+        put("10fps", "10");
+    }};
+    private final Map<String, String> empty = new HashMap<String, String>() {{
+        put("empty", "empty");
+    }};
+
 
     public ExpandableListAdapter(Activity context, List<String> parent, Map<String, List<String>> childs) {
         this.context = context;
@@ -52,7 +123,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
 
     @Override
     public View getChildView(final int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        final String laptop = (String) getChild(groupPosition, childPosition);
+        final String child = (String) getChild(groupPosition, childPosition);
         LayoutInflater inflater = context.getLayoutInflater();
 
         int childType = getChildType(groupPosition, childPosition);
@@ -74,6 +145,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
                     break;
                 case CHILD_TYPE_3:
                     Log.d(TAG, "ChildType 3");
+
                     convertView = inflater.inflate(R.layout.childitem_dropdownchooser_camsettingsactivity, parent, false);
                     convertView.setTag(childType);
                     break;
@@ -91,6 +163,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         else {
             // There is nothing to do here really we just need to set the content of view which we do in both cases
         }
+        TextView txt;
         switch (childType) {
             case CHILD_TYPE_1:
                 /*TextView description_child = (TextView) convertView.findViewById(R.id.description_of_ads_expandable_list_child_text_view);
@@ -98,19 +171,51 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
                 setup_AEB(convertView);
                 break;
             case CHILD_TYPE_2:
-                Spinner spinner = (Spinner)convertView.findViewById(R.id.sp_ddch_spinner);
-
+                //
+                txt = (TextView) convertView.findViewById(R.id.tv_chbx_discription);
+                txt.setText(child);
                 //Define how to render the data on the CHILD_TYPE_2 layout
                 break;
             case CHILD_TYPE_3:
+                Spinner spinner = (Spinner) convertView.findViewById(R.id.sp_ddch_spinner);
+                spinner.setOnItemSelectedListener(this);
+                txt = (TextView) convertView.findViewById(R.id.tv_ddch_discription);
+                txt.setText(child);
+                ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(context, android.R.layout.simple_spinner_item);
+                //if Image Settings
+                if (groupPosition == 1) {
+                    if (childPosition == 0) {
+                        adapter.addAll(aspectRatio.keySet().toArray(new CharSequence[0]));
+                    } else if (childPosition == 1) {
+                        adapter.addAll(imageSize.keySet().toArray(new CharSequence[0]));
+                    } else if (childPosition == 2) {
+                        //CharSequence[] player_names = players.keySet().toArray(new CharSequence[0]);
+                        adapter.addAll(jpgCompression.keySet().toArray(new CharSequence[0]));
+                    } else if (childPosition == 3) {
+                        adapter.addAll(imageSaveDestination.keySet().toArray(new CharSequence[0]));
+                    } else {
+                        adapter.addAll(empty.keySet().toArray(new CharSequence[0]));
+                    }
+
+                } //if movie settings
+                else if (groupPosition == 2) {
+                    if (childPosition == 0) {
+                        adapter.addAll(movieQuality.keySet().toArray(new CharSequence[0]));
+                    } else if (childPosition == 1) {
+                        adapter.addAll(clipRecordTime.keySet().toArray(new CharSequence[0]));
+                    }
+                }
+
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+                // Apply the adapter to the spinner
+                spinner.setAdapter(adapter);
                 //Define how to render the data on the CHILD_TYPE_3 layout
                 break;
             case CHILD_TYPE_UNDEFINED:
                 //Define how to render the data on the CHILD_TYPE_UNDEFINED layout
                 break;
         }
-
-
         return convertView;
     }
 
@@ -123,6 +228,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
     public int getChildTypeCount() {
         return 4; // I defined 4 child types (CHILD_TYPE_1, CHILD_TYPE_2, CHILD_TYPE_3, CHILD_TYPE_UNDEFINED)
     }
+
     public int getChildType(int groupPosition, int childPosition) {
         switch (groupPosition) {
             case 0:
@@ -134,25 +240,31 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
             case 1:
                 switch (childPosition) {
                     case 0:
-                        return CHILD_TYPE_3;
+                        return CHILD_TYPE_3;//AspectRatio
+                    case 1:
+                        return CHILD_TYPE_3;//ImageSize
+                    case 2:
+                        return CHILD_TYPE_3;//jpgCompression
+                    case 3:
+                        return CHILD_TYPE_3;//ImageDestination
+                    case 4:
+                        return CHILD_TYPE_2;//RawImageSaving
                 }
                 break;
             case 2:
-                break;
-            case 3:
-                break;
-            case 4:
                 switch (childPosition) {
                     case 0:
-                        return CHILD_TYPE_1;
+                        return CHILD_TYPE_3;//AspectRatio
+                    case 1:
+                        return CHILD_TYPE_3;//ImageSize
+                    default:
+                        return CHILD_TYPE_UNDEFINED;
                 }
-                break;
-            default:
-                return CHILD_TYPE_UNDEFINED;
         }
-
         return CHILD_TYPE_UNDEFINED;
+
     }
+
     //group-----------------
 
     @Override
@@ -171,15 +283,16 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
     }
 
     @Override
-    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        String laptopName = (String) getGroup(groupPosition);
+    public View getGroupView(int groupPosition, boolean isExpanded, View
+            convertView, ViewGroup parent) {
+        String itemName = (String) getGroup(groupPosition);
         if (convertView == null) {
             LayoutInflater infalInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = infalInflater.inflate(R.layout.groupitem_camsettingsactivtiy, null);
         }
         TextView item = (TextView) convertView.findViewById(R.id.tv_parent);
         item.setTypeface(null, Typeface.BOLD);
-        item.setText(laptopName);
+        item.setText(itemName);
         return convertView;
     }
 
@@ -198,7 +311,8 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         final String[] expSprVal = {"1", "2", "3"};
         setupNumberPicker(np_exposureSpreadVal, expSprVal);
     }
-    private void setup_ddch(){
+
+    private void setup_ddch() {
 
     }
 
@@ -223,5 +337,15 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
         return true;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Log.d(TAG, "selected: " + parent.getItemAtPosition(position));
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
